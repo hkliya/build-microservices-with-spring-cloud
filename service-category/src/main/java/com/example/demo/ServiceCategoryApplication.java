aa87c08b43486c0c9119633577b656c8214eba7e
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-24 13:29:19 +0800

    Refresh config online need POST refresh to service/refresh

--- service-category/src/main/java/com/example/demo/ServiceCategoryApplication.java
+++ service-category/src/main/java/com/example/demo/ServiceCategoryApplication.java
@@ -1,16 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ServiceCategoryApplication {
+	@Value("${config.welcome: no message}")
+	private String welcome;
+
+	@GetMapping("/welcome")
+	String welcome() {
+		return this.welcome;
+	}
+
 	@GetMapping("/categories")
 	List<Category> categories() {
 		return Arrays.asList(
